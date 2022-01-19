209baab06adc2bad3d064976e8876a3dce843bef
nkoshkina <nkoshkina@gmail.com>
2022-01-19 17:00:44 +0300

    added 1 fixture for all into file conftest.py

--- fixture/application.py
+++ fixture/application.py
@@ -19,6 +19,9 @@ class Application:
         wd = self.wd
         wd.get("http://localhost/addressbook/")
 
+    def open_home_link(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("home").click()
 
 
     def destroy(self):
--- fixture/navigation.py
+++ fixture/navigation.py
@@ -3,11 +3,5 @@ class NavigationHelper:
     def __init__(self, app):
         self.app = app
 
-    def open_home_page(self):
-        wd = self.app.wd
-        wd.get("http://localhost/addressbook/")
 
 
-    def open_home_link(self):
-        wd = self.app.wd
-        wd.find_element_by_link_text("home").click()

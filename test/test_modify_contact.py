94e81a3febca3f84747be21be57a11f3170d8b57
nkoshkina <nkoshkina@gmail.com>
2022-01-20 11:40:33 +0300

    added new methods and tests for groups and contacts - updated tests names

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -1,7 +1,7 @@
 from model.contact import Contact
 
 
-def test_add_new_contact(app):
+def test_modify_first_contact(app):
     app.session.login(username="admin", password="secret")
     app.contact.modify_first_contact(Contact("10n", "20n", "30n", "0nick", "0Title", "0Comp", "0address",
                                     "+7911111", "+74560089011", "+790011", "+71123456789",

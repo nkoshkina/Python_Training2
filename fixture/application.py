3b3b0ed29c483a270231d7d1652e8be01c165997
nkoshkina <nkoshkina@gmail.com>
2022-01-24 19:24:45 +0300

    commit according task 16

--- fixture/application.py
+++ fixture/application.py
@@ -5,13 +5,22 @@ from fixture.contact import ContactHelper
 
 
 class Application:
-    def __init__(self):
-        self.wd = webdriver.Firefox()
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "safari":
+            self.wd = webdriver.Safari()
+        elif browser == "edge":
+            self.wd = webdriver.Edge()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.wd.implicitly_wait(1)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
-
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -22,7 +31,7 @@ class Application:
 
     def open_initial_page(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
+        wd.get(self.base_url)
 
 
     def destroy(self):

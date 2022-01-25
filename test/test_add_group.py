bb3dfa5647b146144683573d1d4af76c1b2932eb
nkoshkina <nkoshkina@gmail.com>
2022-01-25 16:51:49 +0300

    data generation: Object of type Group is not JSON serializable

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,18 +1,9 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
 import pytest
-import random
-import string
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+from data.add_group import testdata
 
-testdata = [Group(name="", header="", footer="")] + \
-           [
-    Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20)) \
-    for i in range(2)
-]
 
 @pytest.mark.parametrize("group0", testdata, ids=[repr(x) for x in testdata])
 def test_add_group(app, group0):
